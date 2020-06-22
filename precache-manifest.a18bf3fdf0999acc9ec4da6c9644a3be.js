a30c1bf9a9507a871efa69c917cb94402ef24f40
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-22 14:41:26 -0300

    Updates

---
+++ precache-manifest.a18bf3fdf0999acc9ec4da6c9644a3be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20bb629e18f91a39df193f2cb571193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b3cd53e33879528317",
+    "url": "/static/css/main.1b5f7565.chunk.css"
+  },
+  {
+    "revision": "6a6da4692f0157fd919b",
+    "url": "/static/js/2.37d75ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37d75ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b3cd53e33879528317",
+    "url": "/static/js/main.c22f0a9a.chunk.js"
+  },
+  {
+    "revision": "a9042a044b8b2eafd91a",
+    "url": "/static/js/runtime-main.7c7e9311.js"
+  }
+]);
