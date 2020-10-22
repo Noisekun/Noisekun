f6eff1c87dfea5218cfa5ae35a2e21322842200d
Mateus Felipe <mateusfelipefg77@gmail.com>
2020-10-22 13:49:34 +0000

    Updates

---
+++ precache-manifest.f5141b4cecb0f9143204c2ec7e2b4d0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de3c7a0c50c741dd7f6092f42b35766",
+    "url": "/Noisekun/index.html"
+  },
+  {
+    "revision": "c399aedc6f475060fd32",
+    "url": "/Noisekun/static/js/2.3f7912a9.chunk.js"
+  },
+  {
+    "revision": "d023c84d1f932d047e9acb803e59a84f",
+    "url": "/Noisekun/static/js/2.3f7912a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602079c7aa35586924ed",
+    "url": "/Noisekun/static/js/main.c86389e7.chunk.js"
+  },
+  {
+    "revision": "6426fec3a28d7363680f",
+    "url": "/Noisekun/static/js/runtime-main.25c0d679.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40e812ab3adbcfbd9696dfdc5e18e657.js"
+  "/Noisekun/precache-manifest.f5141b4cecb0f9143204c2ec7e2b4d0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Noisekun/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
