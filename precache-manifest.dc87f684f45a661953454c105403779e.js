ef67951b95fc1ebeba583f66bdf63b9883406f25
Mateus Felipe <mateusfelipefg77@gmail.com>
2020-11-12 18:56:14 +0000

    Updates

---
+++ precache-manifest.dc87f684f45a661953454c105403779e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478fc3660cee9e5609958530105392e5",
+    "url": "/Noisekun/index.html"
+  },
+  {
+    "revision": "1524eaa7824083ba65e1",
+    "url": "/Noisekun/static/js/2.65f6971a.chunk.js"
+  },
+  {
+    "revision": "d023c84d1f932d047e9acb803e59a84f",
+    "url": "/Noisekun/static/js/2.65f6971a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602079c7aa35586924ed",
+    "url": "/Noisekun/static/js/main.c86389e7.chunk.js"
+  },
+  {
+    "revision": "6426fec3a28d7363680f",
+    "url": "/Noisekun/static/js/runtime-main.25c0d679.js"
+  }
+]);
